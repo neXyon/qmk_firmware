4133c4d561b4f45be54ba64f1e40bc3a8a0c02e8
Jörg Müller <nexyon@gmail.com>
2023-02-11 12:34:31 +0100

    First split keyboard version.

--- keyboards/handwired/nexyon/config.h
+++ keyboards/handwired/nexyon/config.h
@@ -3,6 +3,27 @@
 
 #pragma once
 
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 8
+
+// wiring of each half
+#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { F4, F5, F6, F7, B1, B3, B2, B6 }
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* Set 0 if debouncing isn't needed */
+//#define DEBOUNCE 5
+
+/* Use I2C for communication between the halves */
+#define USE_I2C
+
+/* serial.c configuration for split keyboard */
+//#define SOFT_SERIAL_PIN D0
+
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
@@ -18,3 +39,4 @@
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
+
